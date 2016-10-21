b522e800a92aa360bd0b71cde328180a46dcb573
Stenkilde <hello@stenkil.de>
2016-10-21 10:53:03 +0200

    PogChamp

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Player from './Components/Player';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Player />
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/Components/Player.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import './components.css';
+//import speaker from '../speaker.png';
+import badummMp3 from '../ba_dumm.mp3';
+import badummOgg from '../ba_dumm.ogg';
+
+class Player extends Component {
+
+    playSound() {
+        const audio = document.getElementById('audio');
+        audio.play();
+        console.log('Playing a sound');
+    }
+
+    render() {
+        return (
+            <div className="badum">
+                <h1><b>Bad joke?</b>  Go ahead, tap it!</h1>
+                    <div className="animation-holder">
+                        <div className="speaker">
+                            <button onClick={() => this.playSound()}>Hello</button>
+                            <div className="playbutton"></div>
+                            <div className="wave"></div>
+                            <div className="wave delay"></div>
+                            <div className="wave delay2"></div>
+                        </div>
+                    </div>
+                    <audio id="audio">
+                      <source src={badummOgg} type="audio/ogg" />
+                      <source src={badummMp3} type="audio/mpeg" />
+                        Your browser does not support the audio element.
+                    </audio>
+            </div>
+        );
+    }
+}
+
+export default Player;
